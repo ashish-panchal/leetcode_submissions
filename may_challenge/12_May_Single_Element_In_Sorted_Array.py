188febb440dab63179e4f574d883074f6ffd1676
Ashish Panchal <ashish.u.panchal@gmail.com>
2020-05-13 23:02:09 +0530

    12 May: Single element in sorted array

---
+++ may_challenge/12_May_Single_Element_In_Sorted_Array.py
@@ -0,0 +1,35 @@
+"""
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
+
+
+
+Example 1:
+
+Input: [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+
+Input: [3,3,7,7,10,11,11]
+Output: 10
+
+
+Note: Your solution should run in O(log n) time and O(1) space.
+"""
+
+
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(nums)):
+            is_even = (i % 2 == 0)
+
+            if is_even:
+                count += nums(i)
+            else:
+                count -= nums(i)
+                if count != 0:
+                    return nums[i-1]
