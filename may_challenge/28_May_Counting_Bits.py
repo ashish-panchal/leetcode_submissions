dc45b978872a05cc61430bca56bd1c6eedd94554
Ashish Panchal <ashish.u.panchal@gmail.com>
2020-05-29 01:02:52 +0530

    28th May: Counting Bits

---
+++ may_challenge/28_May_Counting_Bits.py
@@ -0,0 +1,42 @@
+"""
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number
+of 1's in their binary representation and return them as an array.
+
+Example 1:
+
+Input: 2
+Output: [0,1,1]
+Example 2:
+
+Input: 5
+Output: [0,1,1,2,1,2]
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n)
+/possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other
+language.
+"""
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        bits = {"0": "0", "1": "1", "2": "1", "3": "2", "4": "1", "5": "2",
+                "6": "2", "7": "3", "8": "1", "9": "2"}
+
+        result = []
+
+        for i in range(num + 1):
+            if str(i) in bits:
+                result.append(bits[str(i)])
+            else:
+                if i % 2 == 0:
+                    bits[str(i)] = bits[str(i / 2)]
+                    result.append(bits[str(i)])
+                elif i % 2 == 1:
+                    bits[str(i)] = int(bits[str(i / 2)]) + 1
+                    result.append(bits[str(i)])
+        return result
